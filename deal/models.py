747a4b7c5c127dfde21665e7b92df95d83e07b22
ken <hsuanchen@outlook.com>
2018-12-18 16:39:30 +0800

    add model

--- deal/models.py
+++ deal/models.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.timezone import now
-from django.utils.html import format_html
-from imagekit.admin import AdminThumbnail
+from django.contrib.auth.models import User
 
 
 class Deal(models.Model):
@@ -14,11 +13,23 @@ class Deal(models.Model):
     desc = models.CharField(max_length=255)
     expired_time = models.DateTimeField(default=now, blank=True)
     remarks = models.CharField(max_length=255)
+    buyers = models.ManyToManyField(User, verbose_name="list of buyers")
+
+
+# class Item(models.Model):
+#     name = models.CharField(max_length=30)
+#     price = models.FloatField()
+#     amount = models.IntegerField()
+#     buyer = models.ForeignKey()
+#     customer_field_1_val = models.CharField(max_length=10)
+#     customer_field_2_val = models.CharField(max_length=10)
 
 
 class DealAdmin(admin.ModelAdmin):
     readonly_fields = ["card_pic_image"]
     list_display = ['name', "store", "author", "card_pic_image"]
+
+
     def card_pic_image(self, obj):
         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
             url=obj.card_pic.url,
