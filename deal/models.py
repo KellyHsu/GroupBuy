5878953fe1a46a8e5b322c9941d0c0337f5fa724
ken <hsuanchen@outlook.com>
2018-12-19 13:16:18 +0800

    add order model

--- deal/models.py
+++ deal/models.py
@@ -21,12 +21,24 @@ class Item(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
     amount = models.IntegerField(default=0)
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    # buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     customer_field_1_desc = models.CharField(max_length=10, blank=True, null=True)
-    customer_field_1_val = models.CharField(max_length=10, blank=True, null=True)
+    # customer_field_1_val = models.CharField(max_length=10, blank=True, null=True)
     customer_field_2_dec = models.CharField(max_length=10, blank=True, null=True)
-    customer_field_2_val = models.CharField(max_length=10, blank=True, null=True)
+    # customer_field_2_val = models.CharField(max_length=10, blank=True, null=True)
     deal_id = models.ForeignKey(Deal, on_delete=models.CASCADE)
+    # is_paid = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    total = models.FloatField(default=0.)
+    deal_id = models.ForeignKey(Deal, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    customer_field_1_val = models.CharField(max_length=10, blank=True, null=True)
+    customer_field_2_val = models.CharField(max_length=10, blank=True, null=True)
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -47,26 +59,11 @@ class DealAdmin(admin.ModelAdmin):
     def card_pic_image(self, obj):
         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
             url=obj.card_pic.url,
-            width=obj.card_pic.width,
-            height=obj.card_pic.height,
+            width=128,
+            height=128,
         )
         )
 
 
-#
-# class Menu():
-#     deal_id
-#     name
-#     cost_per_item
-#     amount
-#     custom_field1Desc
-#     custom_field1Val
-#     custom_field2Desc
-#     custom_field2Val
-#
-#
-# class UserDeal(models.Model):
-#     user_id
-#     deal_id
 admin.site.register(Deal, DealAdmin)
 admin.site.register(Item, ItemAdmin)
