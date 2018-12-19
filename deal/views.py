c925b831acdc6c2f82cbde9d0d5328d6f8ea770b
kelly <kelly830619@gmail.com>
2018-12-20 02:22:00 +0800

    modal

--- deal/views.py
+++ deal/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from deal.models import Deal
+from deal.models import Deal, Item
 
 # Create your views here.
 def index(request):
     deals = Deal.objects.filter()
-    return render(request, 'deal/index.html', {"deals":deals})
+    items=[]
+    for d in deals:
+        item = Item.objects.filter(deal_id=d)
+        items.append(item)
+    print(items)
+        # print(item[0].name)
+    return render(request, 'deal/index.html', {"deals":deals,"items":items})
