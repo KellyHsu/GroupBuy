0a9bce06c0438eafa8d3357f5cfadedcaa6a0bcd
kelly <kelly830619@gmail.com>
2018-12-20 13:53:15 +0800

    merge to master

--- deal/views.py
+++ deal/views.py
@@ -5,5 +5,12 @@ from .models import Deal
 # Create your views here.
 def index(request):
     deals = Deal.objects.filter()
-    # deals[0].item
-    return render(request, 'deal/index.html', {"deals":deals})
+    items={}
+    # for d in deals:
+    for d in range(0, len(deals)):
+        item = Item.objects.filter(deal_id=deals[d])
+        # items.append(item)
+        items[d] = item
+    print(items)
+        # print(item[0].name)
+    return render(request, 'deal/index.html', {"deals":deals,"items":items})
